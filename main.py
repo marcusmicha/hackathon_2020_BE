f5fc22f69f9ba6c9b1497e7b47acd87a976150bd
edumarg <edumarg@gmx.com>
2020-08-25 20:10:32 +0300

    remove debug mode and clean code

--- main.py
+++ main.py
@@ -15,27 +15,17 @@ cors = CORS(app)
 # GET the picture
 @app.route("/productpicture", methods=["POST"])
 def get_picture():
-    # binary img
-    # file = request.files['image']
-    # file.save('im-received.png')
-    # # Read the image via file.stream
-    # # img = Image.open(file.stream)
-    # response = app.response_class(response=json.dumps(mockdata), status=200, mimetype='application/json')
-    # return response
-    #  except TypeError as error:
-    # print(error)
-
-    # base64 img
     payload = request.json
     imgstring = payload['image']
     imgstring += "=" * ((4 - len(imgstring) % 4) % 4)
     imgdata = base64.b64decode(imgstring)
-    filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+    filename = 'some_image.jpg'
 
     with open(filename, 'wb') as f:
         f.write(imgdata)
     response = app.response_class(response=json.dumps(mockdata), status=200, mimetype='application/json')
     return response
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
