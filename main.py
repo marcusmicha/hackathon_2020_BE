3352038f92b4c7de9db5a8339455697733d5bbd1
edumarg <edumarg@gmx.com>
2020-08-25 17:56:01 +0300

    added get img base64 working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, json
 from flask_cors import CORS
-from PIL import Image
+import base64
 
 mockdata = {"CO2_level": 3.53,
             "city_origin": "Ashkelon",
@@ -15,15 +15,27 @@ cors = CORS(app)
 # GET the picture
 @app.route("/productpicture", methods=["POST"])
 def get_picture():
-    try:
-        file = request.files['image']
-        file.save('im-received.png')
-        # Read the image via file.stream
-        # img = Image.open(file.stream)
-        response = app.response_class(response=json.dumps(mockdata), status=200, mimetype='application/json')
-        return response
-    except TypeError as error:
-        print(error)
+    # binary img
+    # file = request.files['image']
+    # file.save('im-received.png')
+    # # Read the image via file.stream
+    # # img = Image.open(file.stream)
+    # response = app.response_class(response=json.dumps(mockdata), status=200, mimetype='application/json')
+    # return response
+    #  except TypeError as error:
+    # print(error)
+
+    # base64 img
+    payload = request.json
+    imgstring = payload['image']
+    imgstring += "=" * ((4 - len(imgstring) % 4) % 4)
+    imgdata = base64.b64decode(imgstring)
+    filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+    response = app.response_class(response=json.dumps(mockdata), status=200, mimetype='application/json')
+    return response
 
 
 if __name__ == '__main__':
